6499913bfe9d7b32ecf279d17acb28e37e8fd09b
Tristan Noggle <trtn3001@gmail.com>
2024-05-06 08:04:10 -0500

    Co-authored-by: ScubaFishy <TheScubaFishy@users.noreply.github.com>

--- SUMMATIVE_3.3.1.py
+++ SUMMATIVE_3.3.1.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+import turtle
 import tkinter
 from tkinter import *
 from tkinter import ttk
@@ -7,4 +7,23 @@ import pyautogui
 
 # TODO add math "advertisement" where when you die, it makes you solve a polynomial
 # TODO make hangman for english as well as other minigames
+# TODO make a turtle race
 
+# TKINTER
+root = Tk()
+root.title("SUMMATIVE 3.3.1")
+
+# VARIABLES
+reply = StringVar()
+answer = ""
+
+# FUNCTIONS
+def submit():
+    pass
+
+# OBJECTS
+label = Label(root, text="SOLVE A PROBLEM")
+entry = Entry(root, textvariable=reply)
+submit = Button(root, text="Submit", command=submit)
+
+turtle.done()
